b0160db397b871b1d9e3dda7b51503da2b87580f
willyc <willcowendrums@gmail.com>
2021-12-13 14:44:27 +0000

    variables progress

--- src/variables/assignment.js
+++ src/variables/assignment.js
@@ -7,7 +7,7 @@ firstNumber = 0
 firstNumber = 20
 
 // TODO: Change the code below so that the tests pass
-const secondNumber = 0 // edit this value
+const secondNumber = 42 // edit this value
 
 // do not edit the exported object.
 module.exports = {
