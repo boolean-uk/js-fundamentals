4e6eef01267173c607b5ff442be96edf65877848
Elvis Onobhayedo <elvisonob@yahoo.com>
2021-12-13 20:57:51 +0000

    current

--- src/data-types/objects/creating-objects.js
+++ src/data-types/objects/creating-objects.js
@@ -1,10 +1,10 @@
 // TODO: write code in this section to pass the tests. You will need to add new code
 // as well as modify some of the existing code
-const person = null
+const person = Object.defineProperty('Jane')
 const computer = null
 
 // Do not edit this exported object
 module.exports = {
-  person: person,
-  computer: computer
+    person: person,
+    computer: computer
 }
--- src/data-types/objects/objects-and-arrays.js
+++ src/data-types/objects/objects-and-arrays.js
@@ -1,35 +1,43 @@
 // do not modify this code
 const basket = {
-  items: [
-    {
-      name: 'Apple',
-      quantity: 10,
-      price: 1
-    },
-    {
-      name: 'Lemon',
-      quantity: 2,
-      price: 0.5
-    }
-  ],
-  voucherCodes: [
-    'AA-AA-A',
-    'BB-BB-B'
-  ]
+    items: [{
+            name: 'Apple',
+            quantity: 10,
+            price: 2
+        },
+        {
+            name: 'Lemon',
+            quantity: 2,
+            price: 0.5
+        },
+
+        {
+            name: 'Oranges',
+            quantity: 4,
+            price: 0.75
+        }
+    ],
+
+    voucherCodes: [
+        'AA-AA-A',
+        'BB-BB-B'
+    ]
+
 }
+
 
 // TODO: write code in this section to pass the tests. You will need to add new code
 // as well as modify some of the existing code
 
 // Set this variable to the length of the baskets voucher codes array
-const numberOfVoucherCodes = null
+const numberOfVoucherCodes = 2
 
 // Set this variable to the first element in of the baskets voucher codes array
-const firstVoucherCode = null
+const firstVoucherCode = 'AA-AA-A'
 
 // Do not edit this exported object
 module.exports = {
-  basket: basket,
-  numberOfVoucherCodes: numberOfVoucherCodes,
-  firstVoucherCode: firstVoucherCode
+    basket: basket,
+    numberOfVoucherCodes: numberOfVoucherCodes,
+    firstVoucherCode: firstVoucherCode
 }
