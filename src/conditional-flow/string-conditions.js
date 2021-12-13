e40262047298c3944933be468918c957e9f66cc5
Nico <nicolapicchio@gmail.com>
2021-12-13 19:12:49 +0000

    started conditional-flow exercises

--- src/conditional-flow/string-conditions.js
+++ src/conditional-flow/string-conditions.js
@@ -1,32 +1,42 @@
 // This function should return true if the passed string is equal to "Hello"
 function isHello (val1) {
-
   // TODO: write code in this function body to pass the tests
-
+  if (val1 === 'Hello') {
+    return true
+  } else {
+    return false
+  }
 }
 
 // This function should return true if the passed string is not equal to "Hello"
 function isNotHello (val1) {
-
   // TODO: write code in this function body to pass the tests
-
+  if (val1 !== 'Hello') {
+    return true
+  } else {
+    return false
+  }
 }
 
 // This function should return true if the string val1 is is longer
 // than string val2
 function isLongerThan (val1, val2) {
-
   // TODO: write code in this function body to pass the tests
-
+  if (val1.length > val2.length) {
+    return true
+  } else {
+    return false
+  }
 }
 
 // This function should return true if the string passed in the function's first
 // argument has an odd number of vowels
+const vowels = ['a', 'e', 'i', 'o', 'u']
+let count = 0
 
 function hasOddNumberVowels (val1) {
-
   // TODO: write code in this function body to pass the tests
-
+  
 }
 
 // this function should return the middle character of a string if it has an odd number
