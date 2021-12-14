300bbeda91e9fe142707cc018a9906da99041f6b
Safia <safia.ibrahim2121@gmail.com>
2021-12-14 16:02:37 +0000

    Exercise: creating objects - completed.

--- src/data-types/objects/creating-objects.js
+++ src/data-types/objects/creating-objects.js
@@ -1,10 +1,15 @@
 // TODO: write code in this section to pass the tests. You will need to add new code
 // as well as modify some of the existing code
-const person = null
-const computer = null
+const person = new Object();
+const computer = new Object();
+
+person.name = "Jane";
+person.age = 32;
+computer.form = "laptop";
+computer.specs = { memory: "16GB", storage: "1TB" };
 
 // Do not edit this exported object
 module.exports = {
   person: person,
-  computer: computer
-}
+  computer: computer,
+};
