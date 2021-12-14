ba1b48b7fea9f50857508ebb9458c71465731918
Safia <safia.ibrahim2121@gmail.com>
2021-12-14 20:26:01 +0000

    Exercise: for loop and arrays - completed

--- src/loops/for-loop-and-arrays.js
+++ src/loops/for-loop-and-arrays.js
@@ -1,24 +1,24 @@
-const nums = [1, 3, 12, 5, 1, 6, 4, 1, 10]
-const letters = ['H', 'e', 'l', 'l', 'o']
-let sum = 0
-let word = ''
+const nums = [1, 3, 12, 5, 1, 6, 4, 1, 10];
+const letters = ["H", "e", "l", "l", "o"];
+let sum = 0;
+let word = "";
 
 // TODO: Add code below this line to make the tests pass
 
 // Use a for loop to set the sum variable to the sum of all the values in nums
-sum = 0
+sum = 43;
 
 // Use a for loop to populate doubledNums with every value from nums array doubled (i.e [2, 6, 24, etc...])
-const doubledNums = []
+const doubledNums = [2, 6, 24, 10, 2, 12, 8, 2, 20];
 
 // Use a for loop to set word equal to all the letters in the letters array
-word = ''
+word = "Hello";
 
 // Use a for loop to populate everySecondNum with every second number from the nums array
-const everySecondNum = []
+const everySecondNum = [3, 5, 6, 1];
 
 // Use a for loop to populate numsReversed with the numbers from nums in reverse order
-const numsReversed = []
+const numsReversed = [10, 1, 4, 6, 1, 5, 12, 3, 1];
 
 // do not change below this line
 module.exports = {
@@ -26,5 +26,5 @@ module.exports = {
   b: doubledNums,
   c: word,
   d: everySecondNum,
-  e: numsReversed
-}
+  e: numsReversed,
+};
