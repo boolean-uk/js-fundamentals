c64675b7bd5820e3dc68b18514fb280be6ede2b0
George T <n35.nnlpd@slmail.me>
2022-01-05 21:07:39 +0000

    completed for-loop-and-arrays

--- src/loops/for-loop-and-arrays.js
+++ src/loops/for-loop-and-arrays.js
@@ -6,7 +6,9 @@ let word = ''
 // TODO: Add code below this line to make the tests pass
 
 // Use a for loop to set the sum variable to the sum of all the values in nums
-sum = 0
+for (let i=0; i<nums.length; i++) {
+  sum += (nums[i])
+}
 
 // Use a for loop to populate doubledNums with every value from nums array doubled (i.e [2, 6, 24, etc...])
 const doubledNums = []
