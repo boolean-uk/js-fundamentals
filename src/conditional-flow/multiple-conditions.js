219678d9f5608376abdf555829713fe9009e5bb7
George T <n35.nnlpd@slmail.me>
2022-01-06 11:06:42 +0000

    Completed multiple-conditions

--- src/conditional-flow/multiple-conditions.js
+++ src/conditional-flow/multiple-conditions.js
@@ -1,19 +1,21 @@
 // This function should return true if num is greater
 // than or equal to lower AND less than or equal to upper.
 // Implement this with a single condition.
-function isInRange (num, lower, upper) {
-
+function isInRange(num, lower, upper) {
   // TODO: write code in this function body to pass the tests
-
+  if ((num >= lower) && (num <= upper)) 
+  return true
+  else return false
 }
 
 // This function should return true if the passed string is equal
 // to "Hello" or "Goodbye". Implement this with a single
 // if statement.
-function isHelloOrGoodbye (val1) {
-
+function isHelloOrGoodbye(val1) {
   // TODO: write code in this function body to pass the tests
-
+  if (val1 === "Hello" || val1 === "Goodbye") 
+  return true
+  else return false
 }
 
 // This function should return a string that describes the provided age value. The
@@ -28,13 +30,41 @@ function isHelloOrGoodbye (val1) {
 // 5-12   | Child
 // 13-19  | Teenager
 // 20+    | Adult
-function getAgeDescription (age) {
-
+function getAgeDescription(age) {
   // TODO: write code in this function body to pass the tests
+  switch (age) {
+    case 0:
+      return "Baby";
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+      return "Toddler";
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+    case 12:
+      return "Child";
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+      return "Teenager";
+    break;
+    default:
+      return "Adult"
+  }
 }
 
 module.exports = {
   a: isInRange,
   b: isHelloOrGoodbye,
-  c: getAgeDescription
-}
+  c: getAgeDescription,
+};
