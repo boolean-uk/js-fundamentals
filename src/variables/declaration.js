040fe34b545a3331565b02dd935a55f7a97ae8b7
Alex Jennings <aj0312@my.bristol.ac.uk>
2022-03-03 12:43:51 +0100

    complete declaration

--- src/variables/declaration.js
+++ src/variables/declaration.js
@@ -4,10 +4,10 @@
 // TODO: Declare the variables firstName and age so that the tests pass
 
 // do not edit below this line
-let firstNameExport = ''
+let firstNameExport = 'Alex'
 try { firstNameExport = firstName } catch (e) {}
 
-let ageExport = 0
+let ageExport = 31
 try { ageExport = age } catch (e) {}
 
 module.exports = {
