27c560c75362e67682ca9c83f1d7508406981e2d
Gui Rodrigues <guirocrpe@gmail.com>
2022-03-03 12:22:41 +0100

    EX: variables/declaration

--- src/variables/declaration.js
+++ src/variables/declaration.js
@@ -4,13 +4,19 @@
 // TODO: Declare the variables firstName and age so that the tests pass
 
 // do not edit below this line
-let firstNameExport = ''
-try { firstNameExport = firstName } catch (e) {}
+let firstNameExport = "";
+const firstName = "Jane";
+try {
+  firstNameExport = firstName;
+} catch (e) {}
 
-let ageExport = 0
-try { ageExport = age } catch (e) {}
+let ageExport = 0;
+const age = 35;
+try {
+  ageExport = age;
+} catch (e) {}
 
 module.exports = {
   firstName: firstNameExport,
-  age: ageExport
-}
+  age: ageExport,
+};
