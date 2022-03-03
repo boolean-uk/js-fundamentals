63355ad12a457ddf128fb73649b3afa8b50ff95b
Paulo Barata <pbara7a@gmail.com>
2022-03-03 16:22:17 +0000

    Object keys exercise, passing tests.

--- src/data-types/objects/object-keys.js
+++ src/data-types/objects/object-keys.js
@@ -14,13 +14,28 @@ const book = {
 const isbn13 = '978-0132350884'
 
 // TODO: write code in this section to pass the tests. You will need to add new code
-// as well as modify some of the existing code
+// as well as modify some of the existing code 
 
 // Set this to the book name
-const name = ''
+const name = book['name']
 
 // Set this to the isbn 10 value
-const isbn10 = ''
+const isbn10 = book['isbn']['isbn10']
+
+// Set book's category to Programming
+book['category'] = 'Programming'
+
+// Add key value pair to book
+book['pages'] = 464
+
+// Add key value pair to isbn (nested in book)
+book['isbn']['isbn13'] = isbn13
+
+// Delete key from book
+delete book['dimensions']
+
+// Delete key from isbn (nested in book)
+delete book['isbn']['asin']
 
 // Do not edit this exported object
 module.exports = {
