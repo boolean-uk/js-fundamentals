bd2c00cc5326689f69bc0bef4f15db9feaa59a35
Chris Hyde <c_hyde333@hotmail.com>
2022-03-03 13:42:21 +0000

    Ex: Data-Types. Numbers.js complete

--- spec/data-types/example.spec.js
+++ spec/data-types/example.spec.js
@@ -2,19 +2,19 @@ const { a, b, c, d } = require('../../src/data-types/example')
 
 describe('Example', () => {
 
-  it('upperCaseHello is HELLO', () => {
+  it('upperCaseHello is HELLO', (Hello) => {
     expect(a).toEqual('HELLO')
   })
 
-  it('secondCharacterOfDog is o', () => {
+  it('secondCharacterOfDog is o', (dog) => {
     expect(b).toEqual('o')
   })
 
-  it('numberOfVowels is 5', () => {
+  it('numberOfVowels is 5', (aeiou) => {
     expect(c).toEqual(5)
   })
 
-  it('numOneMultipliedByNumTwo is 200', () => {
+  it('numOneMultipliedByNumTwo is 200', (1 * 2) => {
     expect(d).toEqual(200)
   })
 
