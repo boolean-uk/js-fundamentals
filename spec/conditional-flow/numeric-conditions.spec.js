df2465abe4f761229edaf3efd97b0defff8e37f1
sigristarisa <arisasigrist.ch@gmail.com>
2022-03-03 13:04:40 +0100

    passed the test for numeric-conditions.js

--- spec/conditional-flow/numeric-conditions.spec.js
+++ spec/conditional-flow/numeric-conditions.spec.js
@@ -1,10 +1,10 @@
-const {a, b, c} = require('../../src/conditional-flow/numeric-conditions')
+const { a, b, c } = require('../../src/conditional-flow/numeric-conditions')
 
-describe("Numeric Conditions isArrayEmpty:", () => {
-  it("[] is empty", () => {
+describe('Numeric Conditions isArrayEmpty:', () => {
+  it('[] is empty', () => {
     expect(a([])).toEqual(true)
   })
-  it("[1] is not empty", () => {
+  it('[1] is not empty', () => {
     expect(a([1])).toEqual(false)
   })
 
@@ -13,34 +13,34 @@ describe("Numeric Conditions isArrayEmpty:", () => {
   })
 })
 
-describe("Numeric conditions isGreaterThan:", () => {
-  it("3 is greater than 2", () => {
+describe('Numeric conditions isGreaterThan:', () => {
+  it('3 is greater than 2', () => {
     expect(b(3, 2)).toEqual(true)
   })
 
-  it("0 is not greater than 10", () => {
+  it('0 is not greater than 10', () => {
     expect(b(0, 10)).toEqual(false)
   })
 
-  it("42 is not greater than 42", () => {
+  it('42 is not greater than 42', () => {
     expect(b(42, 42)).toEqual(false)
   })
 
-  it("-1 is greater than -3", () => {
+  it('-1 is greater than -3', () => {
     expect(b(-1, -3)).toEqual(true)
   })
 })
 
-describe("Numeric Conditions findLowest:", () => {
-  it("1 is lowest in [10, 8, 4, 1, 8]", () => {
+describe('Numeric Conditions findLowest:', () => {
+  it('1 is lowest in [10, 8, 4, 1, 8]', () => {
     expect(c([10, 8, 4, 1, 8])).toEqual(1)
   })
 
-  it("-10 is lowest in [0, 0, -10]", () => {
+  it('-10 is lowest in [0, 0, -10]', () => {
     expect(c([0, 0, -10])).toEqual(-10)
   })
 
-  it("100 is lowest in [100,100,100,100]", () => {
+  it('100 is lowest in [100,100,100,100]', () => {
     expect(c([100, 100, 100])).toEqual(100)
   })
 })
--- src/conditional-flow/numeric-conditions.js
+++ src/conditional-flow/numeric-conditions.js
@@ -2,23 +2,28 @@
 
 // This function should return true if there are no elements in the array, false otherwise
 function isArrayEmpty (array) {
-
+  if (array === []) {
+    return true
+  } else {
+    return false
+  }
   // TODO: write code in this function body to pass the tests
-
 }
 
 // This function should return true if num1 is greater than num2, false otherwise
 function isGreaterThan (num1, num2) {
-
+  if (num1 > num2) {
+    return true
+  } else {
+    return false
+  }
   // TODO: write code in this function body to pass the tests
-
 }
 
 // This function should return the lowest number in the passed array
 function findLowest (nums) {
-
+  return Math.min(...nums)
   // TODO: write code in this function body to pass the tests
-
 }
 
 module.exports = {
