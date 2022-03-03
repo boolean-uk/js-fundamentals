d44556a384fc03cf954a3ab49ebf69d81e1750f0
Toby Carlo <tobycarlo.c@gmail.com>
2022-03-03 11:29:59 +0000

    variable assignment 1 complete

--- src/variables/assignment.js
+++ src/variables/assignment.js
@@ -1,11 +1,15 @@
 // do not edit this
-let firstNumber = 10
+let firstNumber = 20
 firstNumber = 0
 
 // TODO: Set the value of firstNumber below so the tests pass
 
+console.log("FIRST NUMBER: ", firstNumber)
+
+firstNumber = 20
+
 // TODO: Change the code below so that the tests pass
-const secondNumber = 0 // edit this value
+const secondNumber = 42// edit this value
 
 // do not edit the exported object.
 module.exports = {
