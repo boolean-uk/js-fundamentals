7b274aca7bdcf4f2079278363ff9fa99fdee15d3
David <czdavid93@gmail.com>
2022-03-03 19:15:00 +0100

    data-types/objects/creating-objects

--- src/data-types/objects/creating-objects.js
+++ src/data-types/objects/creating-objects.js
@@ -8,3 +8,20 @@ module.exports = {
   person: person,
   computer: computer
 }
+
+// Person should be an object with the name "Jane"
+person.name = "Jane"
+
+//Person should be an object with the age 32'
+person.age = 32
+
+
+//Computer should be an object with the form "laptop"
+computer.form = "laptop"
+
+
+//Computer.specs should be an object with a memory "16GB" and storage "1TB"
+computer.specs {
+  memory: "16GB",
+  storage: "1TB"
+}
