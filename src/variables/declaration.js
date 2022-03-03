1422485862500ff351a466a6ac1f266d9070fea7
Dan <dannorthcott8@gmail.com>
2022-03-03 11:48:58 +0000

    declarations assignment completed

--- src/variables/declaration.js
+++ src/variables/declaration.js
@@ -2,13 +2,15 @@
 //
 //
 // TODO: Declare the variables firstName and age so that the tests pass
-
+let firstName = `Jane`
+let age = 35
 // do not edit below this line
 let firstNameExport = ''
 try { firstNameExport = firstName } catch (e) {}
 
 let ageExport = 0
 try { ageExport = age } catch (e) {}
+//try { tries to run this code } if there's an error it is 'caught' and defined as e?
 
 module.exports = {
   firstName: firstNameExport,
