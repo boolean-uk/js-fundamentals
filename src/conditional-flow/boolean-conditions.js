b95d53b468d4cfbcdc562d3dcbe396ded3a450f8
sigristarisa <arisasigrist.ch@gmail.com>
2022-03-03 12:43:31 +0100

    passed the test for boolean-conditions.js

--- src/conditional-flow/boolean-conditions.js
+++ src/conditional-flow/boolean-conditions.js
@@ -2,9 +2,12 @@
 // "Well done, you passed!" if the value is true, or "Sorry, try again"
 // if the value is false.
 function getResult (didPass) {
-
-  // TODO: write code in this function body to pass the tests
-
+// TODO: write code in this function body to pass the tests
+  if (didPass === true) {
+    return 'Well done, you passed!'
+  } else if (didPass === false) {
+    return 'Sorry, try again'
+  }
 }
 
 module.exports = {
