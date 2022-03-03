3e01ee4ad3b955ac714cf9f7a18b06b7fa8eee51
sigristarisa <arisasigrist.ch@gmail.com>
2022-03-03 16:26:52 +0100

    passed the test for creating-objects.js

--- spec/data-types/objects/creating-objects.spec.js
+++ spec/data-types/objects/creating-objects.spec.js
@@ -10,10 +10,10 @@ describe('Creating Objects:', () => {
   })
 
   it('Computer should be an object with the form "laptop"', () => {
-    expect(answers.computer.form).toEqual("laptop")
+    expect(answers.computer.form).toEqual('laptop')
   })
 
   it('Computer.specs should be an object with a memory "16GB" and storage "1TB"', () => {
-    expect(answers.computer.specs).toEqual({memory: "16GB", storage: "1TB"})
+    expect(answers.computer.specs).toEqual({ memory: '16GB', storage: '1TB' })
   })
 })
