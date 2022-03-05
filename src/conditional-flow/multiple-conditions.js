993d5e92e341d9c15aabd4600bb9d97f36284428
David <czdavid93@gmail.com>
2022-03-05 11:28:09 +0100

    boolean conditions completed

--- src/conditional-flow/multiple-conditions.js
+++ src/conditional-flow/multiple-conditions.js
@@ -2,19 +2,31 @@
 // than or equal to lower AND less than or equal to upper.
 // Implement this with a single condition.
 function isInRange (num, lower, upper) {
-
+  if (num>=lower && num<=upper) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
   // TODO: write code in this function body to pass the tests
 
-}
+
 
 // This function should return true if the passed string is equal
 // to "Hello" or "Goodbye". Implement this with a single
 // if statement.
 function isHelloOrGoodbye (val1) {
-
+  if (val1="Hello" || "Goodbye"){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
   // TODO: write code in this function body to pass the tests
 
-}
+
 
 // This function should return a string that describes the provided age value. The
 // table below shows for each range of age values what string should be returned.
@@ -28,10 +40,28 @@ function isHelloOrGoodbye (val1) {
 // 5-12   | Child
 // 13-19  | Teenager
 // 20+    | Adult
+
 function getAgeDescription (age) {
+  if (age = 0) {
+    return "Baby"
+  }
+  else if (age >= 1 && age <= 4) {
+    return "Toddler"
+  }
+  else if (age > 5 && age < 13) {
+    return "Child"
+  }
+  else if (age > 12 && age < 20) {
+    return "Teenager"
+  }
+  else {
+    return "Adult"
+  }
+}
+
 
   // TODO: write code in this function body to pass the tests
-}
+
 
 module.exports = {
   a: isInRange,
--- src/conditional-flow/numeric-conditions.js
+++ src/conditional-flow/numeric-conditions.js
@@ -2,24 +2,47 @@
 
 // This function should return true if there are no elements in the array, false otherwise
 function isArrayEmpty (array) {
+  if (array.length === 0) {
+    return true
+  } 
+  else {
+    return false
+  }
+}
 
   // TODO: write code in this function body to pass the tests
 
-}
+
 
 // This function should return true if num1 is greater than num2, false otherwise
 function isGreaterThan (num1, num2) {
+  if (num1 > num2) {
+    return true
+  }
+  else {
+    return false
+  }
+}
 
   // TODO: write code in this function body to pass the tests
 
-}
+
 
 // This function should return the lowest number in the passed array
 function findLowest (nums) {
+  for (i=0; i=nums.length; i++){
+    if (i<0){
+      return i;
+    }
+  
+  }
+
+
+}
 
   // TODO: write code in this function body to pass the tests
 
-}
+
 
 module.exports = {
   a: isArrayEmpty,
