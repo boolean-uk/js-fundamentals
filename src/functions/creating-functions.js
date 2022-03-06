30f5c4e8301bd07fcf95bd5d8db5b03baf34dd01
UnknownCreator1 <90321114+UnknownCreator1@users.noreply.github.com>
2022-03-06 23:46:59 +0000

    functions: creating-functions.js test pass

--- src/functions/creating-functions.js
+++ src/functions/creating-functions.js
@@ -8,6 +8,10 @@
 //
 // TODO: write code below
 
+const plusOne = num => {
+  return num + 1
+}
+
 // Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
 //
@@ -18,6 +22,11 @@
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
+
+const smile = name => {
+  const capitalizedName = name.charAt(0).toUpperCase() + name.slice(1)
+  return 'Hi, ' + capitalizedName + ' :)'
+}
 
 // TODO: change undefined to be the name of the functions you defined
 module.exports = {
