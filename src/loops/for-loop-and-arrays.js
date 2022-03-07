5dd1856b788878b0a6a26d0262e3dec93364a278
UnknownCreator1 <90321114+UnknownCreator1@users.noreply.github.com>
2022-03-07 20:19:44 +0000

    loops: for-loop-and-arrays.js partially test pass

--- src/loops/for-loop-and-arrays.js
+++ src/loops/for-loop-and-arrays.js
@@ -6,13 +6,26 @@ let word = ''
 // TODO: Add code below this line to make the tests pass
 
 // Use a for loop to set the sum variable to the sum of all the values in nums
-sum = 0
+
+for (let i = 0; i < nums.length; i++) {
+  sum = sum + nums[i]
+}
 
 // Use a for loop to populate doubledNums with every value from nums array doubled (i.e [2, 6, 24, etc...])
-const doubledNums = []
+
+const doubledNums = (arr) => {
+  const doubledArr = []
+  for (let j = 0; j < arr.length; j++) {
+    doubledArr.push(arr[j] * 2)
+  }
+  return doubledArr
+}
 
 // Use a for loop to set word equal to all the letters in the letters array
-word = ''
+
+for (let k = 0; k < letters.length; k++) {
+  word = word + letters[k]
+}
 
 // Use a for loop to populate everySecondNum with every second number from the nums array
 const everySecondNum = []
