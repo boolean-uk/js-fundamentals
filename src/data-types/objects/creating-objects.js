983421921506ed91c95b23ec8184bdef54826065
David <czdavid93@gmail.com>
2022-03-08 12:28:26 +0100

    objects correncted

--- src/data-types/objects/creating-objects.js
+++ src/data-types/objects/creating-objects.js
@@ -1,7 +1,24 @@
 // TODO: write code in this section to pass the tests. You will need to add new code
 // as well as modify some of the existing code
-const person = null
-const computer = null
+// Person should be an object with the name "Jane"
+//Person should be an object with the age 32'
+//Computer should be an object with the form "laptop"
+//Computer.specs should be an object with a memory "16GB" and storage "1TB"
+
+
+const person = {
+  name: "Jane",
+  age: 32
+}
+
+const computer =  {
+  form: "laptop",
+  specs: {
+    memory: "16GB",
+    storage: "1TB"
+  }
+}
+
 
 // Do not edit this exported object
 module.exports = {
@@ -9,19 +26,6 @@ module.exports = {
   computer: computer
 }
 
-// Person should be an object with the name "Jane"
-person.name = "Jane"
-
-//Person should be an object with the age 32'
-person.age = 32
 
 
-//Computer should be an object with the form "laptop"
-computer.form = "laptop"
 
-
-//Computer.specs should be an object with a memory "16GB" and storage "1TB"
-computer.specs {
-  memory: "16GB",
-  storage: "1TB"
-}
