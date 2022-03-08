cd4f2b4c20ad79bccd53170ce76a1162c4b41110
Toby Carlo <tobycarlo.c@gmail.com>
2022-03-08 11:16:10 +0000

    multiple-args

--- src/functions/creating-functions-multiple-args.js
+++ src/functions/creating-functions-multiple-args.js
@@ -9,7 +9,13 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
-
+function createArray(lower, upper) {
+  let anArray = []
+  for (let i = lower; i <= upper; i++) {
+    anArray.push(i)
+  }
+  return anArray
+}
 // define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
 // marks appended to the end. The number of exclamation marks should be
@@ -21,9 +27,16 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
+function shout (str, num) {
+  let output = str.toUpperCase()
+  for (let i = 0; i < num; i++) {
+    output += "!"
+  }
+  return output
+}
 
 // change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined // change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  a: createArray, // change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: shout // change undefined to be the name of the function defined to return the string with exclamations (the second todo)
 }
