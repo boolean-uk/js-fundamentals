017f7038242bd15cce8944d0eae19d80843047c6
sigristarisa <arisasigrist.ch@gmail.com>
2022-03-08 12:17:23 +0100

    added the correct answer for numeric-conditions.js

--- src/conditional-flow/numeric-conditions.js
+++ src/conditional-flow/numeric-conditions.js
@@ -1,33 +1,33 @@
 // TODO: Implement the functions below to make the tests pass
 
 // This function should return true if there are no elements in the array, false otherwise
-function isArrayEmpty (array) {
-  if (array === []) {
-    return true
+function isArrayEmpty(array) {
+  if (array.length === 0) {
+    return true;
   } else {
-    return false
+    return false;
   }
   // TODO: write code in this function body to pass the tests
 }
 
 // This function should return true if num1 is greater than num2, false otherwise
-function isGreaterThan (num1, num2) {
+function isGreaterThan(num1, num2) {
   if (num1 > num2) {
-    return true
+    return true;
   } else {
-    return false
+    return false;
   }
   // TODO: write code in this function body to pass the tests
 }
 
 // This function should return the lowest number in the passed array
-function findLowest (nums) {
-  return Math.min(...nums)
+function findLowest(nums) {
+  return Math.min(...nums);
   // TODO: write code in this function body to pass the tests
 }
 
 module.exports = {
   a: isArrayEmpty,
   b: isGreaterThan,
-  c: findLowest
-}
+  c: findLowest,
+};
