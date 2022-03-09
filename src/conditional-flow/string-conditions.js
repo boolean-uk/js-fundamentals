25311074db6ee0d490a5c1a0b1059248f566df4b
UnknownCreator1 <90321114+UnknownCreator1@users.noreply.github.com>
2022-03-09 15:09:35 +0000

    conditional-flow: string-conditions.js test pass

--- src/conditional-flow/string-conditions.js
+++ src/conditional-flow/string-conditions.js
@@ -1,32 +1,40 @@
 // This function should return true if the passed string is equal to "Hello"
 function isHello (val1) {
-
   // TODO: write code in this function body to pass the tests
-
+  // return (val1 === 'Hello')
+  if (val1 === "Hello") return true;
+   return false;
 }
 
 // This function should return true if the passed string is not equal to "Hello"
 function isNotHello (val1) {
-
   // TODO: write code in this function body to pass the tests
-
+  // return (val1 !== 'Hello')
+  if (val1 !== "Hello") return true;
+  return false;
 }
 
 // This function should return true if the string val1 is is longer
 // than string val2
 function isLongerThan (val1, val2) {
-
   // TODO: write code in this function body to pass the tests
-
+  if (val1.length > val2.length) return true;
+  return false;
 }
 
 // This function should return true if the string passed in the function's first
 // argument has an odd number of vowels
 
 function hasOddNumberVowels (val1) {
-
   // TODO: write code in this function body to pass the tests
-
+  const VOWELS = ['a', 'e', 'i', 'o', 'u']
+  let total = 0
+  for (let i = 0; i < val1.length; i++) {
+    if (VOWELS.includes(val1[i].toLowerCase())) {
+      total++
+    }
+  }
+  return Boolean(total % 2)
 }
 
 // this function should return the middle character of a string if it has an odd number
@@ -36,6 +44,16 @@ function hasOddNumberVowels (val1) {
 function getMiddleLetter (val1) {
   // TODO: write code in this function body to pass the tests
 
+  let mid = Math.round(val1.length / 2)
+  let chars = ""
+  if (val1.length % 2 === 0) {
+    chars += val1[mid - 1]
+    chars += val1[mid]
+  }
+  else {
+    chars += val1[mid - 1]
+  }
+  return chars
 }
 
 // This function should return the name of the season for the provided
@@ -48,8 +66,47 @@ function getMiddleLetter (val1) {
 // Autumn - September to November
 // Winter - December to February
 function seasonForMonth (monthName) {
-
   // TODO: write code in this function body to pass the tests
+  switch (monthName) {
+    case (monthName = "January"):
+      return "Winter";
+      break;
+    case (monthName = "February"):
+      return "Winter";
+      break;
+    case (monthName = "December"):
+      return "Winter";
+      break;
+    case (monthName = "March"):
+      return "Spring";
+      break;
+    case (monthName = "April"):
+      return "Spring";
+      break;
+    case (monthName = "May"):
+      return "Spring";
+      break;
+    case (monthName = "June"):
+      return "Summer";
+      break;
+    case (monthName = "July"):
+      return "Summer";
+      break;
+    case (monthName = "August"):
+      return "Summer";
+      break;
+    case (monthName = "September"):
+      return "Autumn";
+      break;
+    case (monthName = "October"):
+      return "Autumn";
+      break;
+    case (monthName = "November"):
+      return "Autumn";
+      break;
+      default:
+      return "";
+  }
 }
 
 module.exports = {
