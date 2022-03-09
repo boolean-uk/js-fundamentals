60100561facd79ed3f41c9c1860db9a8908614fa
sigristarisa <arisasigrist.ch@gmail.com>
2022-03-09 17:33:51 +0100

    refactored objects-and-arrays.js

--- src/data-types/objects/objects-and-arrays.js
+++ src/data-types/objects/objects-and-arrays.js
@@ -2,39 +2,43 @@
 const basket = {
   items: [
     {
-      name: 'Apple',
+      name: "Apple",
       quantity: 10,
-      price: 2
+      price: 1,
     },
     {
-      name: 'Lemon',
+      name: "Lemon",
       quantity: 2,
-      price: 0.5
+      price: 0.5,
     },
-    {
-      name: 'Oranges',
-      price: 0.75,
-      quantity: 4
-    }
   ],
-  voucherCodes: [
-    'AA-AA-A',
-    'BB-BB-B'
-  ]
-}
+  voucherCodes: ["AA-AA-A", "BB-BB-B"],
+};
 
 // TODO: write code in this section to pass the tests. You will need to add new code
 // as well as modify some of the existing code
 
 // Set this variable to the length of the baskets voucher codes array
-const numberOfVoucherCodes = basket.voucherCodes.length
+const numberOfVoucherCodes = basket.voucherCodes.length;
 
 // Set this variable to the first element in of the baskets voucher codes array
-const firstVoucherCode = basket.voucherCodes[0]
+const firstVoucherCode = basket.voucherCodes[0];
+
+// update the price of apple
+basket.items[0].price = 2;
+
+// add oranges
+const orange = {
+  name: "Oranges",
+  quantity: 4,
+  price: 0.75,
+};
+
+basket.items.push(orange);
 
 // Do not edit this exported object
 module.exports = {
   basket: basket,
   numberOfVoucherCodes: numberOfVoucherCodes,
-  firstVoucherCode: firstVoucherCode
-}
+  firstVoucherCode: firstVoucherCode,
+};
